2bc07fb4ebaac09248371d378489dcb88e328790
linwenjun <linwenjun@163.com>
2020-11-27 07:02:53 +0000

    first commit

---
+++ ParkingLotApiTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ParkingLotApi.Repository;
+
+namespace ParkingLotApiTest
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup>
+        where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<ParkingLotContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<ParkingLotContext>(options =>
+                {
+                    InMemoryDbContextOptionsExtensions.UseInMemoryDatabase(options, "InMemoryDbForTesting");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<ParkingLotContext>();
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
---
+++ ParkingLotApiTest/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ParkingLotApi;
+using ParkingLotApi.Repository;
+using Xunit;
+
+namespace ParkingLotApiTest
+{
+    public class TestBase : IClassFixture<CustomWebApplicationFactory<Startup>>, IDisposable
+    {
+        public TestBase(CustomWebApplicationFactory<Startup> factory)
+        {
+            this.Factory = factory;
+        }
+
+        protected CustomWebApplicationFactory<Startup> Factory { get; }
+
+        public void Dispose()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var context = scopedServices.GetRequiredService<ParkingLotContext>();
+
+            context.SaveChanges();
+        }
+
+        protected HttpClient GetClient()
+        {
+            return Factory.CreateClient();
+        }
+    }
+}
